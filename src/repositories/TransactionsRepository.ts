18879d16d17e939bcded65c09b0bb32d1c79c52b
Harca <lucas.harca@gmail.com>
2020-04-20 11:58:57 -0300

    fim do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,21 @@
+import { response } from 'express';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface RetornoTransactions {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -13,16 +25,45 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): RetornoTransactions {
+    const retornoTransaction = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+    return retornoTransaction;
   }
 
   public getBalance(): Balance {
-    // TODO
+    function reduceArray(array: Transaction[]): number {
+      const newArray = array.map(arr => arr.value);
+      const totalValue = newArray.reduce((a, b) => a + b, 0);
+      return totalValue;
+    }
+
+    const incomeArray = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+    const outcomeArray = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const income = reduceArray(incomeArray);
+    const outcome = reduceArray(outcomeArray);
+    const total = income - outcome;
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
